650bc6b8a6f937a6db8622bc469b96171810ad65
orphamiel <orphamiel@yahoo.com>
2019-10-05 17:50:04 +0800

    Added working code for Stock Launcher
    Added working code for Stock Launcher

--- app/src/androidTest/java/com/example/fmlm/AutomateTest.kt
+++ app/src/androidTest/java/com/example/fmlm/AutomateTest.kt
@@ -1,28 +1,18 @@
 package com.example.fmlm
 
-import android.content.pm.ActivityInfo
 import android.os.Environment
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.action.ViewActions
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import androidx.test.uiautomator.UiDevice
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.uiautomator.UiSelector
-import androidx.test.uiautomator.UiObject
 import java.io.File
-import androidx.test.espresso.ViewAction
-import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
-import android.app.Activity
-import android.view.View
-import androidx.test.espresso.UiController
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
-import androidx.test.runner.lifecycle.Stage
-import org.hamcrest.Matcher
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.uiautomator.*
 
 
 @RunWith(AndroidJUnit4::class)
@@ -41,23 +31,32 @@ class AutomateTest {
     @Test
     fun pressHome() {
         val mDevice = UiDevice.getInstance(getInstrumentation())
-        //val a : OrientationChangeAction = OrientationChangeAction()
-        //onView(isRoot()).perform(a.orientationLandscape())
-        //Thread.sleep(100)
-        //rule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-        //for stock launcher but not working
-        //val allAppsButton: UiObject = mDevice.findObject(UiSelector().description("Apps list"))
-        //allAppsButton.swipeUp(100)
-        /*
-        mDevice.pressHome()
-        val recentApp = mDevice.findObject(UiSelector().resourceId("com.android.systemui:id/recent_apps")) //doesn't work
-        recentApp.clickAndWaitForNewWindow()
-        dump_screen()*/
         /**
          * Very device and launcher dependent
          * Dump your screen hierarchy using the getObjects helper function below and check the xml for the right node
+         * Below works for emulator with stock launcher on Android 10
+         */
+        for(i in 1..3){
+            val recent : UiObject2   = mDevice.wait(Until.findObject(By.res("com.android.systemui:id/recent_apps")),5000)
+            recent.click()
+            val recentApps = mDevice.findObject(UiSelector().resourceId("com.google.android.apps.nexuslauncher:id/drag_layer"))
+            recentApps.clickAndWaitForNewWindow()
+            onView(ViewMatchers.withId(R.id.edittext_name)).perform(ViewActions.typeText(i.toString())).perform(
+                ViewActions.closeSoftKeyboard())
+        }
+        for(i in 1..3){
+            mDevice.pressHome()
+            val appViews = mDevice.findObject(UiSelector().resourceId("com.google.android.apps.nexuslauncher:id/workspace"))
+            appViews.swipeUp(10)
+            val app : UiObject2   = mDevice.wait(Until.findObject(By.text("FMLM")),5000)
+            app.click()
+            onView(ViewMatchers.withId(R.id.edittext_name)).perform(ViewActions.typeText(i.toString())).perform(
+                ViewActions.closeSoftKeyboard())
+        }
+        /**
          * Below works for Nova Launcher on the Pixel 2 XL
          */
+        /*
         for(i in 1..3) {
             mDevice.pressHome()
             val allAppsButton: UiObject = mDevice.findObject(UiSelector().text("Apps"))
@@ -68,38 +67,7 @@ class AutomateTest {
                 appViews.swipeLeft(5)
             }
             ourApp.clickAndWaitForNewWindow()
-        }
-    }
-
-    inner class OrientationChangeAction private constructor(private val orientation: Int) :ViewAction {
-
-        override fun getConstraints(): Matcher<View> {
-            return isRoot()
-        }
-
-        override fun getDescription(): String {
-            return "change orientation to $orientation"
-        }
-
-        override fun perform(uiController: UiController, view: View) {
-            uiController.loopMainThreadUntilIdle()
-            val activity = view.getContext() as Activity
-            activity.requestedOrientation = orientation
-
-            val resumedActivities =
-                ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
-            if (resumedActivities.isEmpty()) {
-                throw RuntimeException("Could not change orientation")
-            }
-        }
-
-        fun orientationLandscape(): ViewAction {
-            return OrientationChangeAction(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
-        }
-
-        fun orientationPortrait(): ViewAction {
-            return OrientationChangeAction(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-        }
+        }*/
     }
 
     /**
